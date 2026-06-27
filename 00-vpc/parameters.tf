939d7c10075b55fb1976b4e82ff2c44f58512f3e
maruti swiggy <marutiswiggy@gmail.com>
2026-06-28 01:08:34 +0530

    robo-dev-env

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -4,3 +4,10 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc.vpc_id
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "private_sub_ids" {
+  name  = "/${var.project}/${var.environment}/private_ids"
+  type  = "String"
+  value = join("," ,module.vpc.private_ids)
+  overwrite = true
+}
