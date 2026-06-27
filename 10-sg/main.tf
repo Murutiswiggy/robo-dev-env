55c25f95c0661b3f09bdbc5338836616a1870b3f
maruti swiggy <marutiswiggy@gmail.com>
2026-06-28 02:28:49 +0530

    robo-dev-env

---
+++ 10-sg/main.tf
@@ -0,0 +1,7 @@
+module "sg" {
+    source = "git::https://github.com/Murutiswiggy/robo-dev-sg.git?ref=main"
+    project = var.project
+    environment = var.environment
+    vpc_id = local.vpc_id
+    sg_name = var.sg_name
+}
