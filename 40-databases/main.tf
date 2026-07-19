6ca7113e9c431b36fd854221090d9792a00faaaa
maruti swiggy <marutiswiggy@gmail.com>
2026-07-20 02:11:45 +0530

    roboinfra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -39,11 +39,10 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo bash -x /tmp/bootstrap.sh mongodb ${var.environment}"
+      "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
-
 
 
 
