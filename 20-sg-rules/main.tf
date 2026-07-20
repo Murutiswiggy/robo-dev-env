294874834630b66ffe96786130feb81d7cb7d01f
maruti swiggy <marutiswiggy@gmail.com>
2026-07-20 14:55:14 +0530

    newupdate

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -333,6 +333,15 @@ resource "aws_security_group_rule" "bastion_SSH" {
 }
 
 
+resource "aws_security_group_rule" "bastion_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.bastion_sg_id
+}
+
 
 
 
