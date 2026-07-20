dd469c865422822978331007f74bf74c3b82ef3f
maruti swiggy <marutiswiggy@gmail.com>
2026-07-20 15:30:25 +0530

    changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-component=$1
-environment=$2
-dnf install ansible -y
-mkdir -p /var/log/roboshop/ansible
-chown -R ec2-user:ec2-user /var/log/roboshop
-chmod -R 755 /var/log/roboshop
-touch /var/log/roboshop/ansible.log
+sudo component=$1
+sudo environment=$2
+sudo dnf install ansible -y
+sudo mkdir -p /var/log/roboshop/ansible
+sudo chown -R ec2-user:ec2-user /var/log/roboshop
+sudo chmod -R 755 /var/log/roboshop
+sudo touch /var/log/roboshop/ansible.log
 
-cd /home/ec2-user
-git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
-cd robo-ansible-v3
-ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+sudo cd /home/ec2-user
+sudo git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
+sudo cd robo-ansible-v3
+sudo ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
 
 # #!/bin/bash
 
