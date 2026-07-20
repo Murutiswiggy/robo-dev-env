9e2e7cfed2935ac33391b90006a5a558b0653994
maruti swiggy <marutiswiggy@gmail.com>
2026-07-20 16:27:21 +0530

    changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,17 +1,17 @@
-# #!/bin/bash
+#!/bin/bash
 
-# sudo component=$1
-# sudo environment=$2
-# sudo dnf install ansible -y
-# sudo mkdir -p /var/log/roboshop/ansible
-# sudo chown -R ec2-user:ec2-user /var/log/roboshop
-# sudo chmod -R 755 /var/log/roboshop
-# sudo touch /var/log/roboshop/ansible.log
+component=$1
+environment=$2
+dnf install ansible -y
+mkdir -p /var/log/roboshop/ansible
+chown -R ec2-user:ec2-user /var/log/roboshop
+chmod -R 755 /var/log/roboshop
+touch /var/log/roboshop/ansible.log
 
-# sudo cd /home/ec2-user
-# sudo git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
-# sudo cd roboshop-ansible-v3
-# sudo ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
+cd /home/ec2-user
+git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
+cd roboshop-ansible-v3
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
 
 # #!/bin/bash
 
@@ -50,28 +50,3 @@
 
 # cd robo-ansible-v3
 # ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
-
-#!/bin/bash
-set -euxo pipefail
-
-component=$1
-environment=$2
-
-dnf install -y git ansible-core
-
-mkdir -p /var/log/roboshop
-touch /var/log/roboshop/ansible.log
-
-cd /home/ec2-user
-
-rm -rf robo-ansible-v3
-git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
-
-cd robo-ansible-v3
-
-ansible-playbook \
-  -i localhost, \
-  -c local \
-  -e component="$component" \
-  -e environment="$environment" \
-  roboshop.yaml
