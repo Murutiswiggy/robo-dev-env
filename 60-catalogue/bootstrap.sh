a5355090c94b406d7a86b3780760a2b0f8b9bf84
maruti swiggy <marutiswiggy@gmail.com>
2026-07-27 15:57:05 +0530

    update bootstrap

---
+++ 60-catalogue/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+component=$1
+environment=$2
+dnf install ansible -y
+mkdir -p /var/log/roboshop/ansible
+chown -R ec2-user:ec2-user /var/log/roboshop
+chmod -R 755 /var/log/roboshop
+touch /var/log/roboshop/ansible.log
+
+cd /home/ec2-user
+git clone https://github.com/Murutiswiggy/robo-ansible-v3.git
+cd robo-ansible-v3
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
