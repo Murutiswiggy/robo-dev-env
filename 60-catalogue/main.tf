b1a0334c7d361b135346dafeb54925349d4fcfb2
maruti swiggy <marutiswiggy@gmail.com>
2026-07-30 15:06:01 +0530

    update catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -13,3 +13,39 @@ tags = merge(
     local.common_tags
 )
 }
+
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
+    ]
+  }
+}
+
+
+
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [terraform_data.catalogue]
+
+}
