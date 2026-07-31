2bedb8446667ad99c14e4bcb2e3dd07a741d6858
maruti swiggy <marutiswiggy@gmail.com>
2026-07-31 15:55:34 +0530

    changes

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -6,6 +6,9 @@ data "aws_ssm_parameter" "private_sub_ids" {
    name = "/${var.project}/${var.environment}/private_sub_ids"
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
 
 data "aws_ami" "redhat_ami"{
   most_recent      = true
--- 60-catalogue/local.tf
+++ 60-catalogue/local.tf
@@ -1,6 +1,7 @@
 locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.redhat_ami.id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 
     common_name = "${var.project}-${var.environment}"
     private_sub_id = split("," , data.aws_ssm_parameter.private_sub_ids.value)[0]
