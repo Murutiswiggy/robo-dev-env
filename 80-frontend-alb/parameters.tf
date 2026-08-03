861884406d2c267f2b99db03eba99fbd3228db37
maruti swiggy <marutiswiggy@gmail.com>
2026-08-03 16:49:04 +0530

    add frontend-alb

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -2,6 +2,6 @@
 resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
   type  = "StringList"
-  value = aws_lb_listener.https.arn
+  value = aws_lb_listener.HTTPS.arn
   overwrite = true
 }
