0b245fd8838cbcaafff7807bb411eb98d9f2327f
maruti swiggy <marutiswiggy@gmail.com>
2026-08-06 16:09:11 +0530

    updatinginfra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -3,5 +3,6 @@ module "components" {
   source = "git::https://github.com/Murutiswiggy/robo-component.git?ref=main"
   environment = var.environment
   component = each.key
-  app_version = each.value
+  app_version = each.value.app_version
+  rule_priority = each.value.rule_priority
 } 
