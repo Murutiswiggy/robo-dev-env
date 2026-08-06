9eb41ec137c0aba4163541367d2cfe7583e2dcff
maruti swiggy <marutiswiggy@gmail.com>
2026-08-06 15:53:50 +0530

    updatinginfra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-  source = "git::https://https://github.com/Murutiswiggy/robo-component.git"
+  source = "git::https://https://github.com/Murutiswiggy/robo-component.git?ref=main"
   environment = var.environment
   component = each.key
   app_version = each.value
