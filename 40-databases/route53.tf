dfaddf7d769a6743a14a0fbc6ebcda70ff2b708a
maruti swiggy <marutiswiggy@gmail.com>
2026-08-12 16:00:54 +0530

    change

--- 40-databases/route53.tf
+++ 40-databases/route53.tf
@@ -1,11 +1,11 @@
-resource "aws_route53_record" "mongodb" {
-  zone_id = var.zone_id
-  name    = "mongodb-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = 1
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = var.zone_id
+#   name    = "mongodb-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = 1
 
-  records = [aws_instance.mongodb.private_ip]
-}
+#   records = [aws_instance.mongodb.private_ip]
+# }
 
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
@@ -16,20 +16,20 @@ resource "aws_route53_record" "redis" {
   records = [aws_instance.redis.private_ip]
 }
 
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone_id
-  name    = "mysql-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = 1
+# resource "aws_route53_record" "mysql" {
+#   zone_id = var.zone_id
+#   name    = "mysql-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = 1
 
-  records = [aws_instance.mysql.private_ip]
-}
+#   records = [aws_instance.mysql.private_ip]
+# }
 
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = var.zone_id
-  name    = "rabbitmq-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = 1
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = var.zone_id
+#   name    = "rabbitmq-${var.environment}.${var.domain_name}"
+#   type    = "A"
+#   ttl     = 1
 
-  records = [aws_instance.rabbitmq.private_ip]
-}
+#   records = [aws_instance.rabbitmq.private_ip]
+# }
