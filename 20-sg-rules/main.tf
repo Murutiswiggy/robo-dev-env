0e468cf7efeb51dbd02b56f917583bf7121f7089
maruti swiggy <marutiswiggy@gmail.com>
2026-08-12 14:46:03 +0530

    change

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -219,8 +219,8 @@ resource "aws_security_group_rule" "backend_frontend" {
 
 resource "aws_security_group_rule" "backend_bastion" {
   type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_alb_sg_id
