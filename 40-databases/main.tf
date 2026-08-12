e0b82d5027f64cfd1027b606e51736d78e22743f
maruti swiggy <marutiswiggy@gmail.com>
2026-08-12 16:24:07 +0530

    change

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,45 +1,45 @@
-# resource "aws_instance" "mongodb" {
-#   ami                         = data.aws_ami.redhat_ami.id
-#   instance_type               = "t3.micro"
-#   subnet_id                   = local.database_sub_id
-#   vpc_security_group_ids      = [local.mongodb_sg_id]
-#   associate_public_ip_address = true
+resource "aws_instance" "mongodb" {
+  ami                         = data.aws_ami.redhat_ami.id
+  instance_type               = "t3.micro"
+  subnet_id                   = local.database_sub_id
+  vpc_security_group_ids      = [local.mongodb_sg_id]
+  associate_public_ip_address = true
 
 
-# tags = merge(
-#     {
-#         Name = "${local.common_name}-mongodb"
-#     },
-#     local.common_tags
-# )
-# }
+tags = merge(
+    {
+        Name = "${local.common_name}-mongodb"
+    },
+    local.common_tags
+)
+}
 
 
-# #provisioners
-# resource "terraform_data" "mongodb" {
-#   triggers_replace = [
-#     aws_instance.mongodb.id
-#   ]
+#provisioners
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
 
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     password = "DevOps321"
-#     host        = aws_instance.mongodb.private_ip
-#   }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.mongodb.private_ip
+  }
 
-#   provisioner "file" {
-#     source      = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstrap.sh",
-#       "sudo bash /tmp/bootstrap.sh mongodb ${var.environment}"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo bash /tmp/bootstrap.sh mongodb ${var.environment}"
+    ]
+  }
+}
 
 
 
