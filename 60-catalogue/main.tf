1a8f2c0d38cf939bb20f9bdfd87ebf1c6d9d92c9
maruti swiggy <marutiswiggy@gmail.com>
2026-08-17 01:20:06 +0530

    changes

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -213,11 +213,6 @@ resource "aws_lb_listener_rule" "catalogue" {
     target_group_arn = aws_lb_target_group.catalogue.arn
   }
 
-  condition {
-    path_pattern {
-      values = ["/static/*"]
-    }
-  }
 
   condition {
     host_header {
